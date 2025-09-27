fbecadd05edcfd676bbe264564164b70cc34c421
Harshavardhan <pharshavardhanreddy81166@gmail.com>
2025-09-27 22:33:30 +0530

    feat: add frontend deployment configuration

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // Remove any trailing slashes from the API URL to prevent double slashes
-    apiUrl: (process.env.REACT_APP_API_URL || 'http://localhost:8080').replace(/\/$/, ''),
+    apiUrl: (process.env.REACT_APP_API_URL || 'https://jobmatchplatform-server.azurewebsites.net').replace(/\/$/, ''),
 };
 
 export default config;
