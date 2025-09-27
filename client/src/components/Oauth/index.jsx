dac21e355f41730a1b8f397146777ae86fe1ef17
Harshavardhan <pharshavardhanreddy81166@gmail.com>
2025-09-27 15:54:58 +0530

    logo

--- client/src/components/Oauth/index.jsx
+++ client/src/components/Oauth/index.jsx
@@ -4,9 +4,9 @@ import config from "../../config";
 const Oauth = () => {
   const handleOAuthLogin = (provider) => {
     const apiUrl = config.apiUrl;
-    if (provider === 'google') {
+    if (provider === "google") {
       window.location.href = `${apiUrl}/api/auth/google`;
-    } else if (provider === 'github') {
+    } else if (provider === "github") {
       window.location.href = `${apiUrl}/api/auth/github`;
     }
   };
@@ -16,25 +16,25 @@ const Oauth = () => {
       <button
         onClick={() => handleOAuthLogin("google")}
         className={styles.oauth_button}
+        aria-label="Login with Google"
       >
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-          alt="Google Logo" 
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_2013_Google.png"
+          alt="Google Logo"
           className={styles.oauth_icon}
         />
-        Continue with Google
       </button>
-      
+
       <button
         onClick={() => handleOAuthLogin("github")}
         className={styles.oauth_button}
+        aria-label="Login with GitHub"
       >
-        <img 
+        <img
           src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-          alt="GitHub Logo" 
+          alt="GitHub Logo"
           className={styles.oauth_icon}
         />
-        Continue with GitHub
       </button>
     </div>
   );
